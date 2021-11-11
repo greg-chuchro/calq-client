1b31ce69dd42894c088628900911147627661798
greg-chuchro <gchuchro@acm.org>
2021-11-12 01:51:24 +0900

    #1 REST requests

---
+++ Calq.Client/CalqObject.cs
@@ -0,0 +1,23 @@
+﻿namespace Calq.Client {
+    public abstract class CalqObject {
+        private readonly CalqObject parent;
+        private readonly string name;
+
+        internal string Path {
+            get {
+                var path = name;
+                var parent = this.parent;
+                while (parent != null) {
+                    path = $"{parent.name}/{path}";
+                    parent = parent.parent;
+                }
+                return path;
+            }
+        }
+
+        protected CalqObject(CalqObject parent, string name) {
+            this.parent = parent;
+            this.name = name;
+        }
+    }
+}
---
+++ Calq.ClientTest/TestService.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable CS0649
+
+using System.Collections.Generic;
+
+namespace Calq.ClientTest {
+    public class TestService {
+        public class Nested {
+            public int a = 1;
+            public int b;
+        }
+
+        public TestService() {
+        }
+
+        public int integer;
+        public bool boolean;
+        public Nested nested = new();
+        public Nested nullNested;
+        public string text = "text";
+        public string nullText;
+    }
+}
