a4270c74dbce4d8be51f167e2ccd5b98bf488dee
greg-chuchro <gchuchro@acm.org>
2021-12-13 23:25:39 +0900

    #14 expose constructors

---
+++ Calq.ClientTest/CalqList.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using Ghbvft6.Calq.Client;
+using System.Collections.Generic;
+
+namespace Ghbvft6.Calq.ClientTest {
+
+    public class CalqList<T> : List<T>, ICalqObject {
+        private ICalqObject? Parent { get; set; }
+        private string? Name { get; set; }
+
+        ICalqObject? ICalqObject.Parent => Parent;
+        string? ICalqObject.Name => Name;
+
+        public CalqList() { }
+
+        internal void Attach(ICalqObject parent, string name) {
+            Parent = parent;
+            Name = name;
+        }
+    }
+}
---
+++ Calq.ClientTest/CalqObject.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using Ghbvft6.Calq.Client;
+
+namespace Ghbvft6.Calq.ClientTest {
+
+    public class CalqObject : ICalqObject {
+        private ICalqObject? Parent { get; set; }
+        private string? Name { get; set; }
+
+        ICalqObject? ICalqObject.Parent => Parent;
+        string? ICalqObject.Name => Name;
+
+        public CalqObject() { }
+
+        internal void Attach(ICalqObject parent, string name) {
+            Parent = parent;
+            Name = name;
+        }
+    }
+}
